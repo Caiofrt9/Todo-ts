6a36feee20e1c8fbaff2972d1152014645ff81c4
Caio F <silvacaio295@gmail.com>
2022-09-05 10:18:46 -0300

    firts commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//Components
+import Footer from './components/Footer';
+import Hearder from './components/Hearder';
+
+//CSS
+import styles from './App.module.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Hearder/>
+        <main className={styles.main}>
+          <h1>Conteúdo...</h1>
+        </main>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Hearder.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from './Header.module.css'
+
+
+const Hearder = () => {
+  return (
+      <header className={styles.header}>
+        <h1>React + Ts Todo</h1>
+      </header>
+  )
+}
+
+export default Hearder
