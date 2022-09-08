146acf116c23c97a14a421e2a6d2616ae7ce980e
Caio F <silvacaio295@gmail.com>
2022-09-08 09:19:16 -0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
-
-//Components
-import Footer from './components/Footer';
-import Hearder from './components/Hearder';
+import React, { useState } from 'react';
 
 //CSS
 import styles from './App.module.css'
 
+//Components
+import Footer from './components/Footer';
+import Hearder from './components/Hearder';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+
+// Interface
+import {ITask} from './interfaces/Task'
+
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
   return (
     <div className="App">
         <Hearder/>
         <main className={styles.main}>
-          <h1>Conteúdo...</h1>
+          <div>
+            <h2>O que você vai fazer ?</h2>
+            <TaskForm btnText='Criar Tarefa' taskList={taskList} setTaskList={setTaskList}/>
+          </div>
+          <div>
+            <h2>Suas tarefas:</h2>
+            <TaskList/>
+          </div>
         </main>
         <Footer/>
     </div>
